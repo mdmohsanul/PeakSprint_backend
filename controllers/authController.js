3df9533ca215b8906177a733ad44c22a498a2e34
Mohsanul786 <mdmohsan2407@gmail.com>
2025-03-29 11:20:44 +0530

    feat:added pending task route

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,7 @@ const signupHandler = async (req, res) => {
   try {
     const existingUser = await userModel.findOne({ email });
     if (existingUser)
-      return res.status(400).json({ error: "User already exists" });
+      return res.status(400).json({ message: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new userModel({
